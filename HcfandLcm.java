40f45b5463bddf482243628ff3f45303abf09032
Anant Kaushik <68469834+AnantkaushikGit@users.noreply.github.com>
2021-10-30 22:33:05 +0530

    HCF and LCM
    Program to input two numbers, find their HCF & LCM.
    Example:  if two numbers are 24 & 42, then there HCF is 6  and  LCM is 168.

---
+++ HcfandLcm.java
@@ -0,0 +1,35 @@
+package Set1Solutions;
+
+import java.util.Scanner;
+
+class HcfandLcm {
+	 public static void main(String[] args) {
+		 int temp1, temp2, num1, num2, temp, hcf, lcm;
+	      Scanner obj = new Scanner(System.in);
+
+	      System.out.print("Enter First Number: ");
+	      num1 = obj.nextInt();
+	      System.out.print("Enter Second Number: ");
+	      num2 = obj.nextInt();
+	      
+
+	      temp1 = num1;
+	      temp2 = num2;
+
+	      while(temp2 != 0){
+	         temp = temp2;
+	         temp2 = temp1%temp2;
+	         temp1 = temp;
+	      }
+
+	      hcf = temp1;
+	      lcm = (num1*num2)/hcf;
+
+	      System.out.println("HCF of input numbers: "+hcf);
+	      System.out.println("LCM of input numbers: "+lcm);
+		
+
+	
+	}
+
+}
